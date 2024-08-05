5db627975ed9ec5df19b890f71f88448aa299163
Nataliia-Homyk324 <homyk324@gmail.com>
2024-08-05 21:36:01 +0300

    login
    видалила закоментований код

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -60,26 +60,7 @@ export const registerUserController = async (req, res) => {
   });
 };
 
-// export const loginUserController = async (req, res) => {
-//   const session = await loginUser(req.body);
 
-//   res.cookie('refreshToken', session.refreshToken, {
-//     httpOnly: true,
-//     expires: new Date(Date.now() + SEVEN_DAY),
-//   });
-//   res.cookie('sessionId', session._id, {
-//     httpOnly: true,
-//     expires: new Date(Date.now() + SEVEN_DAY),
-//   });
-
-//   res.json({
-//     status: 200,
-//     message: 'Successfully logged in an user!',
-//     data: {
-//       accessToken: session.accessToken,
-//     },
-//   });
-// };
 
 export const loginUserController = async (req, res) => {
   const { user, session } = await loginUser(req.body);
