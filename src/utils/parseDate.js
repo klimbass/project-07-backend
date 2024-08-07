e6d7b737a2ab287eae21d9a2b7ab00bc6dc24380
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-08-07 10:26:58 +0200

    swager

--- src/utils/parseDate.js
+++ src/utils/parseDate.js
@@ -2,7 +2,7 @@
 const getCurrentDate = (query, last, regex) => {
     if (!query.date||!query.date.match(regex)){
     const now = new Date();
-    // Приведення дати до рядка у форматі 'YYYY-MM-DD HH:mm'}
+    // Приведення дати до рядка у форматі 'YYYY-MM-DD'}
     return now.toISOString().slice(0, last);
   }
   return query.date;
